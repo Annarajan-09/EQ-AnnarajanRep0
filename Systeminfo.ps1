dd25dd930919d64b847a2498668fda9fa1f4f985
Annarajan-09 <87439660+Annarajan-09@users.noreply.github.com>
2022-09-03 14:23:13 +0530

    Update Systeminfo.ps1

--- Systeminfo.ps1
+++ Systeminfo.ps1
@@ -2,3 +2,4 @@
 Write-Host "Aravind, I'm a Jenkins build!"
 Write-Host "Santhan Sree Lakshmi, I'm a Jenkins build!"
 Write-Host "Santhana Sree Lakshmi, I'm a Jenkins build!"
+Write-Host "Santhana Sree Lakshmi, I'm a Jenkins build!"
