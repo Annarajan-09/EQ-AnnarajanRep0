61ee687d004efe5ad05bed054191d88659db20a9
Annarajan-09 <87439660+Annarajan-09@users.noreply.github.com>
2022-09-03 14:16:43 +0530

    Add files via upload

---
+++ Systeminfo.ps1
@@ -0,0 +1,3 @@
+﻿#Get-WmiObject -Class win32_physicalmemory | Select-Object BankLabel, Capacity, Speed, PartNumber
+Write-Host "Aravind, I'm a Jenkins build!"
+Write-Host "Santhan Sree Lakshmi, I'm a Jenkins build!"
